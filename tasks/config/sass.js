6b3e73fbadcb12e928d273b9710e75ea82399234
Monaz4 <Mon4d@pm.me>
2022-03-20 09:56:14 +0700

    fix sass

--- tasks/config/sass.js
+++ tasks/config/sass.js
@@ -1,19 +1,21 @@
-module.exports = function(grunt) {
-
-  grunt.config.set('sass', {
+module.exports = function (grunt) {
+  grunt.config.set("sass", {
     dev: {
       options: {
+        quiet: true,
         // style: 'compressed',
       },
-      files: [{
-        expand: true,
-        cwd: 'assets/styles/',
-        src: ['importer.scss'],
-        dest: '.tmp/public/styles/',
-        ext: '.css'
-      }]
-    }
+      files: [
+        {
+          expand: true,
+          cwd: "assets/styles/",
+          src: ["importer.scss"],
+          dest: ".tmp/public/styles/",
+          ext: ".css",
+        },
+      ],
+    },
   });
 
-  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks("grunt-contrib-sass");
 };
